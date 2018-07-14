a9c65f4984e4d237b21d26345eee6e63f3fdc44a
awiorko <lukasz.awiorko@gmail.com>
2018-07-14 15:01:52 +0200

    localproxy service

--- moxproxy.localservice/src/main/java/services/LocalMoxProxyService.java
+++ moxproxy.localservice/src/main/java/services/LocalMoxProxyService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class LocalMoxProxyService extends MoxProxyService implements IMoxProxyLocalService {
 
-
     private IProxyServer proxyServer;
 
     @Autowired
