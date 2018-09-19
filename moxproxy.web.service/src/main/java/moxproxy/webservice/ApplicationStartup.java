db4647ac7a883d9ec5847a631b12b53e07e491dd
awiorko <lukasz.awiorko@gmail.com>
2018-09-20 00:48:40 +0200

    crated application starter and fixed pom dependency to google guava

---
+++ moxproxy.web.service/src/main/java/moxproxy/webservice/ApplicationStartup.java
@@ -0,0 +1,22 @@
+package moxproxy.webservice;
+
+import moxproxy.configuration.ServiceBeanConfiguration;
+import moxproxy.interfaces.IMoxProxyServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
+
+@Component
+@Import(ServiceBeanConfiguration.class)
+public class ApplicationStartup implements ApplicationListener<ApplicationReadyEvent> {
+
+    @Autowired
+    IMoxProxyServer proxyServer;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        proxyServer.startServer();
+    }
+}
---
+++ moxproxy.web.service/src/main/java/moxproxy/webservice/MoxProxyWebService.java
@@ -0,0 +1,13 @@
+package moxproxy.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoxProxyWebService {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoxProxyWebService.class, args);
+    }
+}
