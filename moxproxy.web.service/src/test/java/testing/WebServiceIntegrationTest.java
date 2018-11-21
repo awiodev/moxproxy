966c38d1067c2e7f2b9319be0cf5b7489f4eb0f5
Lukasz Awiorko <lukasz.awiorko@pl.abb.com>
2018-11-21 15:33:19 +0100

    client for tests

---
+++ moxproxy.web.service/src/test/java/testing/WebServiceIntegrationTest.java
@@ -0,0 +1,79 @@
+package testing;
+
+import client.WebServiceTestClient;
+import moxproxy.client.MoxProxyClient;
+import moxproxy.configuration.IMoxProxyClientConfiguration;
+import moxproxy.dto.MoxProxyProcessedTrafficEntry;
+import moxproxy.interfaces.IMoxProxyDatabase;
+import moxproxy.webservice.MoxProxyWebService;
+import moxproxy.webservice.config.WebServiceBeanConfiguration;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = MoxProxyWebService.class)
+@AutoConfigureMockMvc
+@Import({WebServiceIntegrationTestConfiguration.class, WebServiceBeanConfiguration.class})
+public class WebServiceIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    MoxProxyClient moxProxyClient;
+
+    @Autowired
+    IMoxProxyClientConfiguration configuration;
+
+    private WebServiceTestClient client;
+
+    @Autowired
+    private IMoxProxyDatabase database;
+
+
+
+    private static ConfigurableApplicationContext context;
+
+    /*@BeforeAll
+    public static void startService(){
+        context = SpringApplication.run(MoxProxyWebService.class);
+    }
+
+    @AfterAll
+    public static void stopService(){
+        context.close();
+    }*/
+
+    //@Test
+    public void test(){
+        Iterable<MoxProxyProcessedTrafficEntry> traffic = moxProxyClient.getAllRequestTraffic();
+    }
+
+
+    @BeforeEach
+    public void before(){
+        client = new WebServiceTestClient(mockMvc, configuration);
+    }
+
+    @Test
+    public void intTest() throws Exception {
+
+        database.addProcessedRequest(new MoxProxyProcessedTrafficEntry());
+
+        ArrayList<MoxProxyProcessedTrafficEntry> traffic = Lists.newArrayList(client.getAllRequestTraffic());
+    }
+}
--- moxproxy.web.service/src/test/java/testing/WebServiceIntegrationTestConfiguration.java
+++ moxproxy.web.service/src/test/java/testing/WebServiceIntegrationTestConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @TestConfiguration
-public class WebServiceE2ETestConfiguration {
+public class WebServiceIntegrationTestConfiguration {
 
     @Bean
     MoxProxyClient moxProxyClient(){
